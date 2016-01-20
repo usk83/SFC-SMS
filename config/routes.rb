377b04c8d41a1eff9ce9e4704f59f769dc50fbc0
Yusuke Takahashi <t13548yt@sfc.keio.ac.jp>
2016-01-20 23:24:26 +0900

    全ユーザー向けindexページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   resources :affiliations
   resources :committees
   get 'welcome/index'
+  resources :home, :only => [ :index ]
 
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
@@ -24,7 +25,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
