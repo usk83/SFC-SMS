e16122b4286172a35c5b9e8d2461753461b731d8
Yusuke Takahashi <t13548yt@sfc.keio.ac.jp>
2016-01-21 05:17:48 +0900

    task_shift周りのビュー他調整

--- app/models/shift_time.rb
+++ app/models/shift_time.rb
@@ -4,4 +4,8 @@ class ShiftTime < ActiveRecord::Base
 
   has_many :task_shifts
   has_many :tasks, :through => :task_shifts
+
+  def shift_datetime
+      self.shift_date.strftime("%Y/%m/%d") + ' ' + self.shift_time.strftime("%H:%M:%S")
+  end
 end
