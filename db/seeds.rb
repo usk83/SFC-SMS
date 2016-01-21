3219fe81079fd232da2ea198572b29075a24c9b2
Yusuke Takahashi <t13548yt@sfc.keio.ac.jp>
2016-01-22 02:05:57 +0900

    開発用シードファイルの変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,4 @@
-AdminUser.create!([
-  {email: "usk@keio.jp", encrypted_password: "$2a$10$zTzVPC2ewWE/43fyArjI/uhVRHg015CZCZwJJHlj2kfEnMUA99tqS", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 10, current_sign_in_at: "2016-01-21 12:23:00", last_sign_in_at: "2016-01-21 10:35:26", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1"}
-])
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 ActiveNight.create!([
   {user_id: 1, allnight_id: 1},
   {user_id: 1, allnight_id: 2}
